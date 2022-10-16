b589f86b4d99a0453d3e212c280e5cde483fccf4
hortlund <hortlund>
2022-10-16 21:41:54 +0200

    Start of ast & parser

---
+++ parser/parser.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"buddy/ast"
+	"buddy/lexer"
+	"buddy/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
+
